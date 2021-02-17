423852d37ceb47b0814aa784e9c39498fb8b8009
daniel.zelazny <daniel.zelazny@edpauto.com>
2021-02-17 14:14:25 +0100

    Define final client

--- src/random_api/random_api_client.go
+++ src/random_api/random_api_client.go
@@ -2,51 +2,103 @@ package random_api
 
 import (
 	"github.com/ybbus/jsonrpc/v2"
-	"sync"
+	"net/http"
 )
 
 var (
-	rpcClientInstance jsonrpc.RPCClient
-	once sync.Once
+	// TODO handle params via .env
+	rpcClientInstance = jsonrpc.NewClient("https://api.random.org/json-rpc/2/invoke")
+	RandomApiClient = &randomApiClient{
+		rpcClientInstance,
+		"7033c3d0-1314-4f3e-9cf5-7944e992ac9f",
+	}
 )
 
-type ApiParams struct {
+type randomApiClient struct{
+	rpcClient interface{CallableClientInterface}
+	apiKey string
+}
+
+type RandomApiClientInterface interface {
+	GetRandomIntegers(numberOfIntegers int, min int, max int) ([]int, interface{ clientErrorInterface })
+}
+
+type CallableClientInterface interface {
+	Call(method string, params ...interface{}) (*jsonrpc.RPCResponse, error)
+}
+
+func (client *randomApiClient) GetRandomIntegers(numberOfIntegers int, min int, max int) ([]int, interface{ clientErrorInterface }) {
+	rpcClient := client.rpcClient
+	response, apiError := rpcClient.Call(
+		"generateIntegers",
+		&apiParams{client.apiKey, numberOfIntegers, min, max},
+	)
+
+	if apiError != nil {
+		return nil, &clientError{
+			code:    http.StatusInternalServerError,
+			message: apiError.Error(),
+		}
+	}
+	if response != nil && response.Error != nil {
+		var code int
+		if response.Error.Code < 0 {
+			// TODO dubious - https://api.random.org/json-rpc/2/error-codes can be mapped more strictly
+			code = http.StatusInternalServerError
+		} else {
+			code = response.Error.Code
+		}
+		return nil, &clientError{
+			code:    code,
+			message: response.Error.Message,
+		}
+	}
+
+	result := &apiResult{Random: apiRandomResult{
+		Data: []int{},
+		CompletionTime: "",
+	}}
+
+	gettingObjectError := response.GetObject(&result)
+	if gettingObjectError != nil {
+		return nil, &clientError{
+			code:    http.StatusInternalServerError,
+			message: gettingObjectError.Error(),
+		}
+	}
+
+	return result.Random.Data, nil
+}
+
+type apiParams struct {
 	ApiKey string `json:"apiKey"`
 	NumberOfIntegers int `json:"n"`
 	Min int `json:"min"`
 	Max int `json:"max"`
 }
 
-type ApiRandomResult struct {
-	Data [5]int
+type apiRandomResult struct {
+	Data []int
 	CompletionTime string
 }
 
-type ApiResult struct {
-	Random ApiRandomResult
+type apiResult struct {
+	Random apiRandomResult
 }
 
-func clientInit() jsonrpc.RPCClient {
-	once.Do(func () {
-		rpcClientInstance = jsonrpc.NewClient("https://api.random.org/json-rpc/2/invoke")
-	})
-
-	return rpcClientInstance
+type clientError struct {
+	code    int
+	message string
 }
 
-func getRandomIntegers(numberOfIntegers int, min int, max int) ([5]int, error) {
-	rpcClient := clientInit()
-	response, _ := rpcClient.Call(
-		"generateIntegers",
-		&ApiParams{"7033c3d0-1314-4f3e-9cf5-7944e992ac9f", numberOfIntegers, min, max},
-	)
+type clientErrorInterface interface {
+	Error() string
+	GetCode() int
+}
 
-	result := &ApiResult{Random: ApiRandomResult{
-		Data: [5]int{},
-		CompletionTime: "",
-	}}
-
-	err := response.GetObject(&result)
-
-	return result.Random.Data, err
+func (error *clientError) Error() string {
+	return error.message
+}
+func (error *clientError) GetCode() int {
+	return error.code
 }
---
+++ src/random_api/random_api_client_test.go
@@ -0,0 +1,167 @@
+package random_api
+
+import (
+	"errors"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/ybbus/jsonrpc/v2"
+	"net/http"
+	"testing"
+)
+
+func TestReturnCorrectnessCallingRpcClientWithSuccess(t *testing.T) {
+	apiCallParams := &apiParams{"some api key", 3, 1,3}
+	expectedData := []int{1,2,3}
+
+	clientMock := prepareMockWithSuccessResponse(apiCallParams, expectedData)
+
+	clientInstance := randomApiClient{
+		clientMock,
+		apiCallParams.ApiKey,
+	}
+
+	result, _ := clientInstance.GetRandomIntegers(
+		apiCallParams.NumberOfIntegers,
+		apiCallParams.Min,
+		apiCallParams.Max,
+	)
+
+	assert.Equal(t, expectedData, result)
+}
+
+func TestApiErrorFromClientCall(t *testing.T) {
+	apiError := errors.New("some api error")
+	apiCallParams := &apiParams{"some api key", 3, 1,3}
+	expectedError := &clientError{
+		code:    http.StatusInternalServerError,
+		message: apiError.Error(),
+	}
+
+	clientMock := prepareMockWithApiError(apiCallParams, apiError)
+
+	clientInstance := randomApiClient{
+		clientMock,
+		apiCallParams.ApiKey,
+	}
+
+	_, apiError = clientInstance.GetRandomIntegers(
+		apiCallParams.NumberOfIntegers,
+		apiCallParams.Min,
+		apiCallParams.Max,
+	)
+
+	assert.Equal(t, expectedError, apiError)
+}
+
+func TestRpcResponseApiErrorFromClientCall(t *testing.T) {
+	apiCallParams := &apiParams{"some api key", 3, 1,3}
+
+	var testData = []struct{
+		rpcApiError *jsonrpc.RPCError
+		expectedClientError *clientError
+	}{
+		{
+			rpcApiError: &jsonrpc.RPCError{
+				Code:    666,
+				Message: "some internal error",
+				Data:    nil,
+			},
+			expectedClientError : &clientError{
+				code:    666,
+				message: "some internal error",
+			},
+		},
+		{
+			rpcApiError: &jsonrpc.RPCError{
+				Code:    -666,
+				Message: "some internal error sourced in negative error code",
+				Data:    nil,
+			},
+			expectedClientError : &clientError{
+				code:    500,
+				message: "some internal error sourced in negative error code",
+			},
+		},
+	}
+
+	for _, data := range testData {
+		clientMock := prepareMockWithResponseRpcError(apiCallParams, data.rpcApiError)
+
+		clientInstance := randomApiClient{
+			clientMock,
+			apiCallParams.ApiKey,
+		}
+
+		_, apiError := clientInstance.GetRandomIntegers(
+			apiCallParams.NumberOfIntegers,
+			apiCallParams.Min,
+			apiCallParams.Max,
+		)
+
+		assert.Equal(t, data.expectedClientError, apiError)
+	}
+}
+
+func prepareMockWithSuccessResponse(apiParams *apiParams, expectedData []int) *Client {
+	rpcResponse := jsonrpc.RPCResponse{
+		JSONRPC: "",
+		Result: &apiResult{Random: apiRandomResult{
+			Data: expectedData,
+			CompletionTime: "",
+		}},
+		Error:   nil,
+		ID:      0,
+	}
+
+	clientMock := new(Client)
+	clientMock.On("Call", "generateIntegers", apiParams).Return(
+		&rpcResponse,
+		nil,
+	).Once()
+
+	return clientMock
+}
+
+func prepareMockWithApiError(apiParams *apiParams, apiError error) *Client {
+	clientMock := new(Client)
+	clientMock.On("Call", "generateIntegers", apiParams).Return(
+		nil,
+		apiError,
+	).Once()
+
+	return clientMock
+}
+
+func prepareMockWithResponseRpcError(apiParams *apiParams, apiError *jsonrpc.RPCError) *Client {
+	rpcResponse := jsonrpc.RPCResponse{
+		JSONRPC: "",
+		Result:  nil,
+		Error:   apiError,
+		ID:      0,
+	}
+
+	clientMock := new(Client)
+	clientMock.On("Call", "generateIntegers", apiParams).Return(
+		&rpcResponse,
+		nil,
+	).Once()
+
+	return clientMock
+}
+
+type Client struct {
+	mock.Mock
+}
+
+func (c *Client) Call(method string, params ...interface{}) (*jsonrpc.RPCResponse, error) {
+	args := c.Called(method, params[0])
+
+	var rpcResponse *jsonrpc.RPCResponse
+	if args.Get(0) == nil {
+		rpcResponse = nil
+	} else {
+		rpcResponse = args.Get(0).(*jsonrpc.RPCResponse)
+	}
+
+	return rpcResponse, args.Error(1)
+}

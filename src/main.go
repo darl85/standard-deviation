d6b908e35836185c6e23edfad79faeec24cbf89c
daniel.zelazny <daniel.zelazny@edpauto.com>
2021-02-17 14:52:37 +0100

    Handle requests in mean handler

--- src/main.go
+++ src/main.go
@@ -1,18 +1,53 @@
 package main
 
 import (
+    "github.com/gorilla/mux"
     "net/http"
+    "standard-deviation/src/calculations"
+    "standard-deviation/src/handler"
+    "standard-deviation/src/numbers"
     "standard-deviation/src/random_api"
 )
 
+type ApiResponseError struct {
+    code    int
+    message string
+}
 
-func hello(writer http.ResponseWriter, request *http.Request) {
-    random_api.getRandomIntegers(5, 1, 100)
+func (responseError *ApiResponseError) Error() string {
+    return responseError.message
+}
+func (responseError *ApiResponseError) GetCode() int {
+    return responseError.code
+}
+
+func meanHandler(writer http.ResponseWriter, request *http.Request) {
+    writer.Header().Set("Content-Type", "application/json")
+
+    requests, numberOfIntegers, paramsValidationError := handler.HandleQueryParameters(request)
+
+    if paramsValidationError != nil {
+        handler.HandleErrorResponse(writer, paramsValidationError)
+        return
+    }
+
+    numberSetsCollection, collectNumberError := numbers.CollectNumberSets(requests, numberOfIntegers, random_api.RandomApiClient)
+    if collectNumberError != nil {
+        handler.HandleErrorResponse(writer, &ApiResponseError{
+            code:    collectNumberError.GetCode(),
+            message: collectNumberError.Error(),
+        })
+        return
+    }
+
+    deviations := calculations.GetStandardDeviations(numberSetsCollection, &calculations.StandardDeviationCalculator)
+    handler.HandleSuccessResponse(writer, deviations)
 }
 
 func main() {
-
-    http.HandleFunc("/hello", hello)
-
-    http.ListenAndServe(":8080", nil)
+    apiRouter := mux.NewRouter().StrictSlash(true)
+    apiRouter.HandleFunc("/random/mean", meanHandler).
+        Methods("GET")
+    http.ListenAndServe(":8080", apiRouter)
 }
+
